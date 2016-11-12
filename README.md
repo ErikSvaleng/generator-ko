b4d5ca22f4ade0cd9ef723f3f48f5fa571edb7ff
ersvale <ersvale@gmail.com>
2016-11-12 14:47:55 +0100

    Simplified project: reverted to ES5 and removed all need for js-transpilation. Added support for LESS ootb. Added some small support for Brackets.io

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# generator-ko
+# Classical-Ko Generator for Yeoman
 
-Generates a starting point for a Knockout application.
+A Yeoman generator that creates a simple starting point for a knockoutjs SPA.
+
+
+## About
+
+This generator is a fork of [the fantastic 'generator-ko'](https://github.com/SteveSanderson/generator-ko) by [the brilliant Steven Sanderson](https://github.com/SteveSanderson).
+
+The purpose of this fork was to simplify the original project, reducing dependencies and avoiding any transpilation of javascript. The aim is to be a simple starting point for a knockoutjs single page application that works out of the box. Also trying to provide a tad more documentation, as the original project is best documented in [this video](https://www.youtube.com/watch?v=I1USsIW8aWE)
+
+There is a minuscule underlying preference for users of the [Brackets.io text editor](http://brackets.io/), but it's absolutely not a dependancy.
+
+## Requirements
+
+Since this is a Yeoman generator you will have to have installed [node.js](https://nodejs.org/en/), which bundles [npm](https://www.npmjs.com/).
+
+And the [Yeoman scaffolding tool](http://yeoman.io/). `npm install -g yo` in node.
+
+## How-to
+
+### Install
+
+When that's all in place, you can install the generator `npm install -g generator-classical-ko`
+
+### Use
+
+**Scaffolding a new app**
+Navigate your terminal to where you want to scaffold up your starting point. Should be an empty folder.
+
+Type `yo classical-ko`
+
+Yeoman will start up and ask you some questions:
+1. What's the name of your app
+2. Do you want to include automated tests? (Karma and Jasmine)
+3. What stylesheet language do you want to use? (Css or Less)
+
+After that, you'll have a small simple app, as a starting point for your project.
+
+**Scaffolding a new component**
+When you need a new component in your app you can scaffold one:
+`yo classical-ko:component <newComponentName>`
+This will add a component in src/components and register it in startup.js
+
+## A small note on choosing LESS
+If you go down the less-route, you can use the `gulp watch` -task to transpile during development, or you can do it "manually" each time with `gulp devLess`.
+If you're using the Brackets.io text editor, I recommend [the LESS AutoCompile extension](https://github.com/jdiehl/brackets-less-autocompile). The style.less file has already been configured for this extension.
+
+## Testing in Brackets.io
+The Karma.conf has Brackets.io added as a reporter, and the karma-brackets package has already been added as a dependency. If you're in to BDD-style development you can simply install [the Karma runner for Brackets](https://github.com/artoale/karma-brackets) and have Karma run your tests directly in Brackets. 
+
+## License
+MIT
--- app/index.js
+++ app/index.js
@@ -4,38 +4,20 @@ var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
+var styleChoice = {
+    css: 'Css',
+    less: 'Less'
+}
+
 var KoGenerator = yeoman.generators.Base.extend({
   init: function () {
     this.pkg = require('../package.json');
 
     this.on('end', function () {
       if (!this.options['skip-install']) {
-        // Figure out whether we have an internet connection. If not, need to
-        // pass --offline to bower otherwise it won't fall back on cache.
-        require('dns').resolve('example.com', function(isOffline) {
-          console.log('Installing dependencies in ' + (isOffline ? 'offline' : 'online') + ' mode...');
-          if (isOffline) {
-            // Patch bowerInstall to pass --offline
-            this.bowerInstall = (function(originalFunction) {
-              return function(paths, options, cb) {
-                options = options || {};
-                options.offline = true;
-                return originalFunction.call(this, paths, options, cb);
-              };
-            })(this.bowerInstall);
-          }
+          console.log('Installing dependencies');
 
-          this.installDependencies();
-
-          if (this.includeTests) {
-            // Install test dependencies too
-            var bowerArgs = ['install'];
-            if (isOffline) {
-              bowerArgs.push('--offline');
-            }
-            this.spawnCommand('bower', bowerArgs, { cwd: 'test' });
-          }
-        }.bind(this));
+          this.npmInstall();
       }
     });
   },
@@ -43,47 +25,52 @@ var KoGenerator = yeoman.generators.Base.extend({
   askFor: function () {
     var done = this.async();
     this.log(this.yeoman);
-    this.log(chalk.magenta('You\'re using the fantastic Knockout app generator.'));
+    this.log(chalk.magenta('Welcome! This generator will create a knockout-project for you.'));
 
     var prompts = [{
-      name: 'name',
-      message: 'What\'s the name of your new site?',
-      default: path.basename(process.cwd())
+        name: 'name',
+        message: 'What\'s the name of your new site?',
+        default: path.basename(process.cwd())
     }, {
-      type: 'confirm',
-      name: 'includeTests',
-      message: 'Do you want to include automated tests, using Jasmine and Karma?',
-      default: true
+        type: 'confirm',
+        name: 'includeTests',
+        message: 'Do you want to include automated tests, using Jasmine and Karma?',
+        default: true
+    }, {
+        type: 'list',
+        name: 'styleLang',
+        message: "What stylesheet language do you want to use?",
+        choices: [styleChoice.css, styleChoice.less]
     }];
 
     this.prompt(prompts, function (props) {
-      this.longName = props.name;
-      this.slugName = this._.slugify(this.longName);
-      this.includeTests = props.includeTests;
-      done();
+        this.longName = props.name;
+        this.slugName = this._.slugify(this.longName);
+        this.usesLess = props.styleLang === 'Less';
+        this.includeTests = props.includeTests;
+        done();
     }.bind(this));
   },
 
   templating: function () {
-    this._processDirectory('src', 'src');
-    this.template('_package.json', 'package.json');
-    this.template('_bower.json', 'bower.json');
-    this.template('_gulpfile.js', 'gulpfile.js');
-    this.template('_gitignore', '.gitignore');
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('jsconfig.json');
+      var excludeExtension = this.usesLess ? '.css' : '.less';
+      this._processDirectory('src', 'src', excludeExtension);
+      this.template('_package.json', 'package.json');
+      this.template('_gulpfile.js', 'gulpfile.js');
+      this.template('_gitignore', '.gitignore');
 
-    if (this.includeTests) {
-      // Set up tests
-      this._processDirectory('test', 'test');
-      this.copy('bowerrc_test', 'test/.bowerrc');
-      this.copy('karma.conf.js');
-    }
+      if (this.includeTests) {
+          // Set up tests
+          this._processDirectory('test', 'test');
+          this.copy('karma.conf.js');
+      }
   },
 
-  _processDirectory: function(source, destination) {
+  _processDirectory: function(source, destination, excludeExtension) {
     var root = this.isPathAbsolute(source) ? source : path.join(this.sourceRoot(), source);
-    var files = this.expandFiles('**', { dot: true, cwd: root });
+    var files = this.expandFiles('**', { dot: true, cwd: root }).filter(function(filename){
+        return !excludeExtension || path.extname(filename) !== excludeExtension;
+    });
 
     for (var i = 0; i < files.length; i++) {
         var f = files[i];

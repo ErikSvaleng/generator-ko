4b52b912981c2541e992155e3e6515d12bda6c5a
ErikSvaleng <ersvale@gmail.com>
2017-02-12 13:33:42 +0100

    Fixed bug with page load

--- app/templates/src/app/components.config.js
+++ app/templates/src/app/components.config.js
@@ -2,8 +2,8 @@
     // Pages - knockout components that serve as pages
     pages: [
         // [Scaffolded pages will be inserted here. To retain this feature, don't remove this comment.]
-        { name: 'home-page', path: 'components/home/home' },
-        { name: 'about-page', path: 'components/about/about' }
+        { name: 'home-page', path: 'pages/home/home' },
+        { name: 'about-page', path: 'pages/about/about' }
     ],
     // Components - knockout components intended as parts of pages, a.k.a. components
     components: [

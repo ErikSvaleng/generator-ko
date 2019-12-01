585c5d94d4c4f9fdf617216e7f6e54c773835117
Alex <ionut.leonte80@yahoo.com>
2019-12-01 19:26:57 +0200

    update to gulp 4 update ko 3.5

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -2,7 +2,7 @@
 var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = require('chalk'), es = require('event-stream')<% if(includeTests){ %>, child = require('child_process')<% } %>;
 
 // Gulp and plugins
-var gulp = require('gulp'), rjs = require('gulp-requirejs-bundler'), concat = require('gulp-concat'), clean = require('gulp-clean'),
+var gulp = require('gulp'), rjs = require('gulp-requirejs'), concat = require('gulp-concat'), clean = require('gulp-clean'),
     replace = require('gulp-replace'), uglify = require('gulp-uglify'), htmlreplace = require('gulp-html-replace'), webserver = require('gulp-webserver')<% if(usesLess) { %>, less = require('gulp-less')<% } %>;
 
 // Config
@@ -95,7 +95,7 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('default', ['html', 'js', <% if(usesLess) { %>'less', <% } else { %>'css', <% } %> 'fonts'], function(callback) {
+gulp.task('default', gulp.series('html', 'js', <% if(usesLess) { %>'less', <% } else { %>'css', <% } %> 'fonts'), function(callback) {
     callback();
     console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
 });
